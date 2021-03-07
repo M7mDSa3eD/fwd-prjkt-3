cc8cec85f817e1d9dfe7ce56df74209833ac04b1
M7mDSa3eD <m7md.sa3ed@hotmail.com>
2021-03-07 11:51:50 +0200

    update url validation testing

--- src/tests/isURL.test.js
+++ src/tests/isURL.test.js
@@ -4,4 +4,12 @@ describe("Testing the checking url validation", () => {
   test("Testing the isURL() function", () => {
     expect(isURL()).toBeDefined();
   });
+
+  test("Return TRUE on valid URL", () => {
+    expect(isURL("https://www.google.com/")).toBe(1);
+  });
+
+  test("Return FALSE on valid URL", () => {
+    expect(isURL("google")).toBe(0);
+  });
 });
